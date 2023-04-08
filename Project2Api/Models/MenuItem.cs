30605b5bb58e34884628905d547ddde99d607119
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-04-08 18:50:26 -0500

    to row

--- Project2Api/Models/MenuItem.cs
+++ Project2Api/Models/MenuItem.cs
@@ -1,3 +1,7 @@
+using System.Data;
+using ErrorOr;
+using Project2Api.ServiceErrors;
+
 namespace Project2Api.Models;
 
 public class MenuItem
@@ -20,5 +24,60 @@ public class MenuItem
         this.Category = category;
         this.Quantity = quantity;
         this.Cutlery = MenuItemCutlery;
+    }
+
+    public static ErrorOr<MenuItem> Create(
+        string name,
+        float price,
+        string category,
+        int quantity,
+        List<string> MenuItemCutlery)
+    {
+        if (name == null || price < 0 || category == null || quantity < 0 || MenuItemCutlery == null)
+        {
+            return ServiceErrors.Errors.MenuItem.InvalidMenuItem;
+        }
+
+        return new MenuItem(name, price, category, quantity, MenuItemCutlery);
+    }
+
+    public static ErrorOr<MenuItem> From(DataRow dataRow)
+    {
+        // check if dataRow is null 
+        if (dataRow == null)
+        {
+            // print error message
+            Console.WriteLine("[OrdersService] Failed to convert data table: data row is null");
+            return Errors.Orders.DbError;
+        }
+
+        // check if any of the columns are null 
+        if (dataRow["id"] == null || dataRow["date_time"] == null || dataRow["total_price"] == null)
+        {
+            // print error message
+            Console.WriteLine("[OrdersService] Failed to convert data table: some fields are null");
+            return Errors.Orders.NotFound;
+        }
+
+        // convert rest of row to order
+        String name = dataRow["name"].ToString() ?? "";
+        String RawPrice = dataRow["price"].ToString() ?? "";
+        String RawCategory = dataRow["category"].ToString() ?? "";
+        String RawQuantity = dataRow["quantity"].ToString() ?? "";
+
+        // create order
+        try
+        {
+            return Create(
+                name,
+                float.Parse(RawPrice),
+                RawCategory,
+                int.Parse(RawQuantity),
+                new List<string>()
+            ); 
+        } catch (System.FormatException e) {
+            Console.Out.WriteLine(e);
+            return Errors.Orders.UnexpectedError;
+        }
     }
 }
