e3651bafb6a7b8b761d21e1f4af571e1f5375078
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-04-09 00:31:25 -0500

    MenuItemServices class comments

--- Project2Api/Services/MenuItem/IMenuItemService.cs
+++ Project2Api/Services/MenuItem/IMenuItemService.cs
@@ -6,12 +6,32 @@ namespace Project2Api.Services.MenuItems
 {
     public interface IMenuItemService
     {
+        /// <summary>
+        /// Creates menu item
+        /// </summary>
+        /// <param name="menuItem"></param>
+        /// <returns>Created menu item</returns>
         ErrorOr<MenuItem> CreateMenuItem(MenuItem menuItem);
-    
+
+        /// <summary>
+        /// Gets menu item by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Menu item</returns>
         ErrorOr<MenuItem> GetMenuItem(string name);
 
+        /// <summary>
+        /// Gets all menu items
+        /// </summary>
+        /// <returns>Dictionary of menu items</returns>
         ErrorOr<Dictionary<string, List<MenuItem>>> GetAllMenuItems();
 
+        /// <summary>
+        /// Updates menu item
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="menuItem"></param>
+        /// <returns>Updated menu item</returns>
         ErrorOr<MenuItem> UpdateMenuItem(string name, MenuItem menuItem);
 
         ErrorOr<IActionResult> DeleteMenuItem(string name);
