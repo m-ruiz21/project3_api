b53e75c52ff6106827de9f44ca9a83f472a1b4e0
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-04-10 08:43:01 -0500

    inventory service and class

---
+++ Project2Api/Models/InventoryItem.cs
@@ -0,0 +1,63 @@
+using System.Data;
+using ErrorOr;
+
+namespace Project2Api.Models;
+
+public class InventoryItem
+{
+    public string name { get; }
+    public string category { get; }
+    public int quantity { get; }
+
+    private InventoryItem(string name, string category, int quantity)
+    {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public ErrorOr<InventoryItem> Create(
+        string name,
+        string category,
+        int quantity
+    )
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(category) || quantity < 0)
+        {
+            return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
+        }
+
+        return new InventoryItem(name, category, quantity);
+    }
+
+    public ErrorOr<InventoryItem> From(DataRow row)
+    {
+        if (!row.Table.Columns.Contains("name") || !row.Table.Columns.Contains("category") || !row.Table.Columns.Contains("quantity"))
+        {
+            return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
+        }
+
+        string name = row["name"].ToString() ?? "";
+        string category = row["category"].ToString() ?? "";
+        string rawQuantity = row["quantity"].ToString() ?? "";
+
+        // make sure none of them are emtpy or null
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(category) || string.IsNullOrEmpty(rawQuantity))
+        {
+            Console.WriteLine("[Inventory] Failed to Convert to Inventory Item: missing values");
+            return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
+        }
+
+        try 
+        {
+            return Create(
+                name,  
+                category, 
+                int.Parse(rawQuantity)
+                );
+        } catch (Exception e) { 
+            Console.WriteLine(e);
+            return ServiceErrors.Errors.Inventory.UnexpectedError;
+        }
+    }
+}
