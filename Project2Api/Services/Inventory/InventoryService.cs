cfdd90f1d649ab13e97dd41a55f16fbbd1d9d5c0
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-04-10 08:47:13 -0500

    init inventory service

---
+++ Project2Api/Services/Inventory/InventoryService.cs
@@ -0,0 +1,27 @@
+using ErrorOr;
+using Project2Api.ServiceErrors;
+using Microsoft.AspNetCore.Mvc;
+using Project2Api.DbTools;
+using Project2Api.Models;
+using System.Data;
+
+namespace Project2Api.Services.Inventory;
+public class MenuItemService : IInventoryService
+{
+    private readonly IDbClient _dbClient;
+
+    public MenuItemService(IDbClient dbClient)
+    {
+        _dbClient = dbClient;
+    }
+
+    public ErrorOr<IActionResult> DeleteInventoryItem(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ErrorOr<List<InventoryItem>> GetAllInventoryItems()
+    {
+        throw new NotImplementedException();
+    }
+}
