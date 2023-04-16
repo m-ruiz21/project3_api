0a84e4342d7827b0c8c43652891f7c6c626fc8d5
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-04-16 15:21:41 -0500

    testing for inventory refactoring

--- Project2Api/Models/InventoryItem.cs
+++ Project2Api/Models/InventoryItem.cs
@@ -36,7 +36,10 @@ public class InventoryItem
         int quantity
     )
     {
-        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(type) || quantity < 0)
+        if (string.IsNullOrEmpty(name) 
+            || (type != "cutlery" && type != "menu item") 
+            || string.IsNullOrEmpty(type) 
+            || quantity < 0)
         {
             return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
         }
@@ -61,41 +64,5 @@ public class InventoryItem
             inventoryRequest.Type,
             0 
         );
-    }
-
-    /// <summary>
-    /// Creates a new InventoryItem from a DataRow
-    /// </summary>
-    /// <param name="row"></param>
-    /// <returns>Error or new inventory item</returns>
-    public static ErrorOr<InventoryItem> From(DataRow row)
-    {
-        if (!row.Table.Columns.Contains("name") || !row.Table.Columns.Contains("type") || !row.Table.Columns.Contains("quantity"))
-        {
-            return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
-        }
-
-        string name = row["name"].ToString() ?? "";
-        string type = row["type"].ToString() ?? "";
-        string rawQuantity = row["quantity"].ToString() ?? "";
-
-        // make sure none of them are emtpy or null
-        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(type) || string.IsNullOrEmpty(rawQuantity))
-        {
-            Console.WriteLine("[Inventory] Failed to Convert to Inventory Item: missing values");
-            return ServiceErrors.Errors.Inventory.InvalidInventoryItem;
-        }
-
-        try 
-        {
-            return Create(
-                name,  
-                type, 
-                int.Parse(rawQuantity)
-                );
-        } catch (Exception e) { 
-            Console.WriteLine(e);
-            return ServiceErrors.Errors.Inventory.UnexpectedError;
-        }
     }
 }
